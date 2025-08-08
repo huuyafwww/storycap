e97185328bae2d0e0fb92e0329624bd95d3bb6b0
huuyafwww <huuya1234fwww@gmail.com>
2025-08-08 20:42:24 +0900

    eslint part update

---
+++ eslint.config.ts
@@ -0,0 +1,17 @@
+import { yaml } from '@huuyafwww/eslint-config-yaml';
+import { define } from '@praha/eslint-config-definer';
+
+import type { Linter } from 'eslint';
+
+const config = define([
+  () => ([{
+    ignores: ['**/*.?([cm])ts?(x)', '**/*.?([cm])js?(x)'],
+    files: [
+      '*.yaml',
+      '*.yml',
+    ],
+  }]),
+  yaml,
+]);
+
+export default config({}) as Linter.Config;
